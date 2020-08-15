94832fb9738c111d8291c555d613fac47165748e
Gramelnet <ninobu.develop@gmail.com>
2020-08-15 11:50:58 +0900

    setting http

--- lib/models/repository/news_repository.dart
+++ lib/models/repository/news_repository.dart
@@ -1,12 +1,49 @@
+import 'package:chopper/chopper.dart';
 import 'package:flutter/material.dart';
 import 'package:newsfeedapp/data/category_info.dart';
 import 'package:newsfeedapp/data/search_type.dart';
+import 'package:newsfeedapp/models/model/news_model.dart';
+import 'package:newsfeedapp/models/networking/api_service.dart';
 
 class NewsRepository {
+  final ApiService _apiService = ApiService.create();
 
-  Future<void> getNews({@required SearchType searchType, String keyword, Category category}) async{
+  Future<List<Article>> getNews(
+      {@required SearchType searchType,
+      String keyword,
+      Category category}) async {
+    List<Article> result = List<Article>();
 
-    print("[Repository]searchType: $searchType / keyword: $keyword / category: ${category.nameJp}");
+    Response response;
 
+    try {
+      switch (searchType) {
+        case SearchType.HEAD_LINE:
+          response = await _apiService.getHeadLines();
+          break;
+        case SearchType.KEYWORD:
+          response = await _apiService.getKeywordNews(keyword: keyword);
+          break;
+        case SearchType.CATEGORY:
+          response =
+              await _apiService.getCategoryNews(category: category.nameEn);
+          break;
+      }
+      if (response.isSuccessful) {
+        final responseBody = response.body;
+        print("responsebody: $responseBody");
+        result = News.fromJson(responseBody).articles;
+      } else {
+        final errorCode = response.statusCode;
+        final error = response.error;
+        print("response is not successful: $errorCode / $error");
+      }
+    } on Exception catch (error) {
+      print("$error");
+    }
+    return result;
+  }
+  void dispose(){
+    _apiService.dispose();
   }
 }
